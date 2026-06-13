002ce83f5a9c6eadae58de8266eccd071f578e9f
matthewzegekwe <matthewzegekwe@gmail.com>
2026-06-13 16:24:52 +0100

    Add personal information logging to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "John Doe";
+const age = 20;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
